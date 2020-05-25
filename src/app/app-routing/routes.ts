98442c32263f1ec1605cac004e3d350731274e0e
suchkovdenis <ny1828@yandex.ru>
2020-05-25 15:19:57 +0300

    week2 single page applications part 1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
 import { MenuComponent } from '../menu/menu.component';
-import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
@@ -11,5 +10,5 @@ export const routes: Routes = [
   {path: 'menu', component: MenuComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: 'contactus', component: ContactComponent}
 ];
