4c61e0676e1fa6b008594618f6abdc7f62923819
suchkovdenis <ny1828@yandex.ru>
2020-07-09 18:35:24 +0300

    week3 Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,9 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -47,6 +49,7 @@ import 'hammerjs';
     BrowserAnimationsModule,
     FlexModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     MatToolbarModule,
     MatListModule,
@@ -57,7 +60,9 @@ import 'hammerjs';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [
     DishService,
